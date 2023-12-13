42dd069e86ebaa21c7aea9bc45d8faebdcecccf1
qidai2000 <94075773+qidai2000@users.noreply.github.com>
2023-12-13 13:22:37 -0500

    update KF

--- KF.py
+++ KF.py
@@ -1,39 +1,18 @@
-import pybullet as p
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-from models import motion_model, sensor_model
 
-# Define the noise covariances
-Q = np.diag([0.01, 0.01, 0.01]) # process noise covariance
-R = np.diag([0.1, 0.1, 0.1]) # measurement noise covariance
+## HW5 code
+def KalmanFilter(mu, Sigma, z, u, A, B, C, Q, R):
+    ###YOUR CODE HERE###
 
-# Define the initial state and control
-x0 = np.array([0, 0, 0]) # initial state
-u = np.array([0.5, 0.1]) # constant control
-dt = 0.1 # time step
+    #prediction step    
+    mu_bar = A @ mu + B @ u
+    Sigma_bar = A @ Sigma @ A.T + R
 
-def kalman_filter(x0, P0, u, z, dt):
-    # x0: initial state estimate
-    # P0: initial state covariance
-    # u: control vector
-    # z: measurement vector
-    # dt: time step
-    # returns: updated state estimate and covariance
-    
-    # Predict step
-    x_pred = motion_model(x0, u, dt)
-    F = np.array([[1, 0, -u[0] * np.sin(x0[2]) * dt],
-                  [0, 1, u[0] * np.cos(x0[2]) * dt],
-                  [0, 0, 1]]) # state transition matrix
-    P_pred = F @ P0 @ F.T + Q # predicted state covariance
-    
-    # Update step
-    y = z - x_pred # innovation
-    H = np.eye(3) # measurement matrix
-    S = H @ P_pred @ H.T + R # innovation covariance
-    K = P_pred @ H.T @ np.linalg.inv(S) # Kalman gain
-    x_upd = x_pred + K @ y # updated state estimate
-    P_upd = (np.eye(3) - K @ H) @ P_pred # updated state covariance
-    
-    return x_upd, P_upd
+    #correction step
+    K = Sigma_bar @ C.T @ np.linalg.inv(C @ Sigma_bar @ C.T + Q)
+    mu_new = mu_bar + K @ (z - C @ mu_bar)
+    Sigma_new = (np.eye(2) - K @ C) @ Sigma_bar
+
+    # mu_new = mu; Sigma_new = Sigma #comment this out to use your code
+    ###YOUR CODE HERE###
+    return mu_new, Sigma_new
