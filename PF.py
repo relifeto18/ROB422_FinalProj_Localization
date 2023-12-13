25bf3181003bb9ae8ffa2aa90089718149f8c788
qidai2000 <94075773+qidai2000@users.noreply.github.com>
2023-12-13 15:05:44 -0500

    PF update

--- PF.py
+++ PF.py
@@ -1,38 +1,58 @@
-import pybullet as p
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from models import motion_model, sensor_model
+from noise import gaussian_noise
+import random
+import copy
 
-def ParticleFilter(X0, W0, u, z, dt, Np):
-    # X0: initial particle set (Np x 3 matrix)
-    # W0: initial weight vector (Np x 1 vector)
-    # u: control vector
-    # z: measurement vector
-    # dt: time step
-    # Np: number of particles
-    # returns: updated particle set and weight vector
-    
-    # Predict step
-    X_pred = np.zeros((Np, 3))
-    for i in range(Np):
-        x = X0[i]
-        x_next = motion_model(x, u, dt) + np.random.multivariate_normal([0, 0, 0], Q)
-        X_pred[i] = x_next
-    
-    # Update step
-    W_upd = np.zeros(Np)
-    for i in range(Np):
-        x = X_pred[i]
-        p_z = stats.multivariate_normal.pdf(z, x, R)
-        W_upd[i] = W0[i] * p_z
-    W_upd = W_upd / np.sum(W_upd) # normalize the weights
-    
-    # Resample step
-    X_upd = np.zeros((Np, 3))
-    indices = np.random.choice(Np, Np, p=W_upd) # resample with replacement
-    for i in range(Np):
-        X_upd[i] = X_pred[indices[i]]
-    W_upd = np.ones(Np) / Np # reset the weights
-    
-    return X_upd, W_upd
+class particle:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+        self.weight = 0
+
+class ParticleFilter:
+    def __init__(self, param: list):
+        self.sample_times = param["Sample_time"]
+        self.particles = []
+        self.sample_cov = param["Sample_cov"]
+        self.dt = param["dt"]
+        for _ in range(self.sample_times):
+            p = particle()
+            p.x, p.y, p.theta = gaussian_noise((0, 0, 0), self.sample_cov)
+            p.weight = 1.0 / self.sample_times
+
+    def PF(self, u: list, z: list):
+        S = []
+        for m in range(self.sample_times):
+            next_particle = particle()
+            next_sample = motion_model(self.particles[m], u[m], self.dt)
+            next_weight = sensor_model(next_sample, z[m])
+
+            next_particle.x = next_sample[0]
+            next_particle.y = next_sample[1]
+            next_particle.theta = next_sample[2]
+            next_particle.weight = next_weight
+            S.append(next_particle)
+        
+        ## resample
+        weight_sum = 0
+        new_particles = copy.deepcopy(self.particles)
+        r = random.uniform(0, 1.0 / self.sample_times)     
+        c = self.particles[0].weight
+        i = 0
+        for m in range(self.sample_times):
+            U = r + m * (1 / self.sample_times)
+
+            while(U > c and i < self.sample_times - 1):
+                i += 1
+                c += self.particles[i].weight
+            new_particles[m] = copy.deepcopy(self.particles[i])
+            weight_sum += self.particles[i].weight
+
+        self.particles = new_particles
+
+        for p in self.particles:
+            p.weight /= weight_sum
