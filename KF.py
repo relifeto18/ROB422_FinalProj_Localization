9e10ff8c17dcfb2cf0bea03f9d71ece44e50eb52
qidai2000 <94075773+qidai2000@users.noreply.github.com>
2023-12-13 15:14:32 -0500

    KF update

--- KF.py
+++ KF.py
@@ -1,17 +1,26 @@
 import numpy as np
 
-## HW5 code
-def KalmanFilter(mu, Sigma, z, u, A, B, C, Q, R):
+class KalmanFilter:
+    def __init__(self, param: list):
+        self.A = param['A']
+        self.B = param['B']
+        self.C = param['C']
+        self.Q = param['Q']
+        self.R = param['R']
+        self.Sigma = np.diag(np.zeros(self.A.shape[0]))
+        
+    ## HW5 code
+    def KalmanFilter(self, mu: list, z: list, u: list):
 
-    #prediction step    
-    mu_bar = A @ mu + B @ u
-    Sigma_bar = A @ Sigma @ A.T + R
+        #prediction step    
+        mu_bar = self.A @ mu + self.B @ u
+        Sigma_bar = self.A @ self.Sigma @ self.A.T + self.R
 
-    #correction step
-    K = Sigma_bar @ C.T @ np.linalg.inv(C @ Sigma_bar @ C.T + Q)
-    mu_new = mu_bar + K @ (z - C @ mu_bar)
-    Sigma_new = (np.eye(2) - K @ C) @ Sigma_bar
+        #correction step
+        K = Sigma_bar @ self.C.T @ np.linalg.inv(self.C @ Sigma_bar @ self.C.T + self.Q)
+        mu_new = mu_bar + K @ (z - self.C @ mu_bar)
+        Sigma_new = (np.eye(2) - K @ self.C) @ Sigma_bar
 
-    # mu_new = mu; Sigma_new = Sigma #comment this out to use your code
-    ###YOUR CODE HERE###
-    return mu_new, Sigma_new
+        # mu_new = mu; Sigma_new = Sigma #comment this out to use your code
+        ###YOUR CODE HERE###
+        return mu_new, Sigma_new
