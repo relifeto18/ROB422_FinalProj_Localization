cb40a145febfd7b83ac26a5d7726040799e0e312
qidai2000 <94075773+qidai2000@users.noreply.github.com>
2023-12-14 17:29:26 -0500

    update PF

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ param = {
     'A': np.eye(3),
     'B': np.eye(3) * 0.1,
     'C': np.eye(3),
-    'Q': np.diag([0.5, 0.5, 0.5]),    # sensor noise
-    'R': np.diag([0.01, 0.01, 0.01]),    # motion noise
+    'Q': np.diag([0.05, 0.05, 0.05]),    # sensor noise
+    'R': np.diag([0.1, 0.1, 0.1]),    # motion noise
     'Sample_time': 300,
     'Sample_cov': np.diag([0.1, 0.1, 0.1])   # covariance of sampling
 }
@@ -71,7 +71,7 @@ def main(screenshot=False):
         motion = motion_input[i]
         mu = tuple(get_joint_positions(robots['pr2'], base_joints))
 
-        u = np.array([motion[0]*np.cos(theta[i]), motion[0]*np.sin(theta[i]), motion[-1]], dtype=float)
+        u = np.array([float(motion[0]*np.cos(theta[i])), float(motion[0]*np.sin(theta[i])), motion[-1]])
         z = sensor_model(path[i+1].copy(), Q)
         mu_new, Sigma_new = KF.KalmanFilter(mu, z, u) 
         KF_error.append(path[i+1] - mu_new)
